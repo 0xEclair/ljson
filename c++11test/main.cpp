5e9e84695b0f40c615a6722b12d3db8379da5048
eclair-lumiere <1164983144l@gmail.com>
2019-07-06 00:01:04 +0800

    Create main.cpp

---
+++ c++11test/main.cpp
@@ -0,0 +1,56 @@
+//#include <iostream>
+//using namespace std;
+//
+//class A {
+//private:
+//	const char* a;
+//public:
+//	const char*& get_a() {
+//		return a;
+//	}
+//	void set(const char* j) {
+//		a = j;
+//	}
+//};
+//
+//void point(const char* a) {
+//	const char tmp[] = "true";
+//	a = tmp;
+//}
+//
+//int main(void) {
+//
+//	A t;
+//	const char b[] = "asdasd";
+//	t.set(b);
+//
+//	auto i = t.get_a();
+//	i++;
+//	t.get_a()++;
+//	std::cout << t.get_a() << std::endl;
+//	const char* a;
+//	point(a);
+//	cout << a << endl;
+//	return 0;
+//}
+#include <iostream>
+class A {
+	union {
+
+	};
+	struct {
+		char* c;
+	};
+};
+void fun(A* tmp) {
+		//like this,note me can't access,but have no error
+		tmp->c = new char('a');
+}
+
+int main(void) {
+
+	A t;
+	fun(&t);
+	std::cout << t.c;
+	return 0;
+}
